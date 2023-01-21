5b0a9a285152e0e949ef520b3886524d967332b8
mknamdeo <mknamdeo@gmail.com>
2023-01-21 23:38:37 +0530

    ch 3.20

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -5,10 +5,16 @@ function Greeting() {
     return(
         <>
             <div className="something">
-                <h2>My Christmas Greetings</h2>
+                <h2><Person/></h2>
+                <h3><Message/></h3>
             </div>
         </>
     )
+}
+
+const Person = () => <h2>Manish Kumar Namdeo</h2>;
+const Message = () => {
+    return <h2>My Christmas Greetings</h2>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
