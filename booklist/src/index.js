c70ba4c6b5bc3b9c366135e1360864625070e717
Manish Kumar Namdeo <mknamdeo@gmail.com>
2023-01-22 16:04:46 +0530

    Booklist ch 3.28 Props

--- booklist/src/index.js
+++ booklist/src/index.js
@@ -3,33 +3,32 @@ import ReactDOM from "react-dom/client";
 
 import './index.css'
 
+const firstBook = {
+    title:"Spare",
+    author:"Prince Harry The Duke of Sussex",
+    img:'https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL900_SR900,600_.jpg'
+}
+
+const secondBook = {
+    title:"Atomic Habits",
+    author:"James Clear",
+    img:'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg'
+}
 const BookList = () => {
   return (<section className="booklist">
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
+    <Book title={firstBook.title} author={firstBook.author} img={firstBook.img} />
+    <Book title={secondBook.title} author={secondBook.author} img={secondBook.img} />
   </section>);
 }
 
-const Book = () => {
+const Book = (props) => {
   return (<article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <img src={props.img} alt={props.title}/>
+    <h2>{props.title}</h2>
+    <h4>{props.author}</h4>
   </article>);
 }
 
-const Image = () => {
-    return <img 
-        src='https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL900_SR900,600_.jpg' 
-        alt='Spare'
-    />
-}
-
-const Title = () => <h2>Spare</h2>
-
-const Author = () => <h4>Prince Harry The Duke of Sussex</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
